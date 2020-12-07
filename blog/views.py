838ea34f84ace7dfe06074ff895940b38fd7582a
fkhanigm <fkhanigm@gmail.com>
2020-12-07 16:04:51 +0330

    changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request): #requesst is create an oject for the view func
+    return render(request, 'blog/post_list.html', {})
